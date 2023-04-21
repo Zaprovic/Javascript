2fed32a147c326e14b983879451789c14f66ee5b
Zaprovic <jazaro2001@gmail.com>
2023-04-21 15:48:32 -0500

    .

---
+++ projects/01-dom/script.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector('h1')
+const thirty = document.querySelector('#thirty-lines');
+const twenty = document.querySelector('.twenty-lines');
+const p = document.querySelector('p');
+const input = document.querySelector("input")
+
+console.log({
+    h1,
+    p,
+    twenty,
+    thirty,
+    input
+
+});
+
+console.log(h1);
+
+
+
+
+
